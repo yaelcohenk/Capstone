22938cdd3bd03ca2d58fa3cc18b180355223f664
FRANCISCO SOLIS <fsolisgormaz@uc.cl>
2024-04-20 16:04:19 -0400

    Actualizacion ADI con datos sin outliers

--- src/adi.py
+++ src/adi.py
@@ -6,19 +6,27 @@ import matplotlib.pyplot as plt
 
 from funciones.adi_categorizacion import category
 
-# pd.options.mode.chained_assignment = None  # default='warn'
-productos_vigentes = pd.read_excel(os.path.join("datos", "prod_vigentes.xlsx"))
-productos_vigentes = productos_vigentes["Descripción"].to_list()
+# Este se tiene que correr sin los outliers. ACTUALIZAR
 
-ventas_diarias_prod = pd.read_excel(os.path.join("datos", "ventas_diarias_productos.xlsx"))
-ventas_diarias_prod = ventas_diarias_prod[ventas_diarias_prod["Descripción"].isin(productos_vigentes)]
 
-productos_unicos = ventas_diarias_prod["Descripción"].unique().tolist()
+pd.options.mode.chained_assignment = None  # default='warn'
+# productos_vigentes = pd.read_excel(os.path.join("datos", "prod_vigentes.xlsx"))
+# productos_vigentes = productos_vigentes["Descripción"].to_list()
+
+# ventas_diarias_prod = pd.read_excel(os.path.join("datos", "ventas_diarias_productos.xlsx"))
+# ventas_diarias_prod = ventas_diarias_prod[ventas_diarias_prod["Descripción"].isin(productos_vigentes)]
+
+# productos_unicos = ventas_diarias_prod["Descripción"].unique().tolist()
+ventas_diarias_prod_vigentes_no_outliers = pd.read_excel(os.path.join("datos", "ventas_diarias_productos_vigentes_no_outliers.xlsx"))
+
+productos_unicos = ventas_diarias_prod_vigentes_no_outliers["Descripción"].unique().tolist()
+
+
 
 listas = list()
 
 for producto in productos_unicos:
-    producto_db = ventas_diarias_prod[ventas_diarias_prod["Descripción"].isin([producto])]
+    producto_db = ventas_diarias_prod_vigentes_no_outliers[ventas_diarias_prod_vigentes_no_outliers["Descripción"].isin([producto])]
     producto_db = producto_db.sort_values(by="Fecha")
     producto_db["diferencia_tiempo"] = producto_db["Fecha"].diff()
     adi = producto_db["diferencia_tiempo"].mean().days
@@ -30,8 +38,7 @@ for producto in productos_unicos:
 dataset = pd.DataFrame(listas, columns=["Descripción", "ADI", "CV2", "Ventas_totales"])
 
 dataset["categoria"] = dataset.apply(category, axis=1)
-# sns.scatterplot(x='CV2',y='ADI',hue='categoria',data=dataset)
-# plt.show()
+
 
 print(dataset.categoria.value_counts())
 dataset.to_excel(os.path.join("datos", "adi_productos.xlsx"), index=False)
---
+++ src/deteccion_outliers.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sys
+import statsmodels.api as sm
+
+from sklearn.ensemble import IsolationForest
+pd.options.mode.chained_assignment = None 
+
+productos_vigentes = pd.read_excel(os.path.join("datos", "prod_vigentes.xlsx"))
+productos_vigentes = productos_vigentes["Descripción"].tolist()
+
+datos = pd.read_excel(os.path.join("datos", "ventas_diarias_productos.xlsx"))
+
+datos = datos[datos["Descripción"].isin(productos_vigentes)]
+
+
+lista_dataframes = list()
+
+print(f"El tamaño de los datos antes de quitar outliers es {datos.shape}")
+
+for producto in productos_vigentes:
+
+    producto_data = datos[datos["Descripción"].isin([producto])]
+    isolation = IsolationForest()
+    producto_data["ISO"] = isolation.fit_predict(producto_data["Cantidad"].values.reshape(-1, 1))
+    producto_data = producto_data[producto_data["ISO"] != -1]
+
+    lista_dataframes.append(producto_data)
+
+
+datos_final = pd.concat(lista_dataframes)
+
+print(f" El tamaño de los datos luego de quitar outliers es {datos_final.shape}")
+
+datos_final.to_excel(os.path.join("datos", "ventas_diarias_productos_vigentes_no_outliers.xlsx"), index=False)
