82a8f35001fc7fed588c554d37c1e0b9b491f95d
FRANCISCO SOLIS <fsolisgormaz@uc.cl>
2024-05-02 20:13:15 -0400

    Comentarios profe

--- src/caso_base.py
+++ src/caso_base.py
@@ -31,6 +31,8 @@ datos_test_venta["item_id"].replace({item_id: nombre_prod}, inplace=True)
 datos_ventas = datos_test_venta[datos_test_venta["item_id"].isin([articulo])]
 datos_ventas = datos_ventas[["item_id", "date", "quantity"]]
 datos_ventas["date"] = pd.to_datetime(datos_ventas["date"])
+
+
 datos_ventas = datos_ventas[datos_ventas["date"].dt.year > 2023]
 
 
@@ -51,20 +53,12 @@ diccionario_demandas = dict()
 for fecha, cantidad in zip(fechas_ventas, cantidad_ventas):
     diccionario_demandas[fecha] = cantidad
 
-def caso_base_T_r_Q(
-        demandas: dict,
-        lista_fechas: list,
-        fecha_min,
-        nombre_prod,
-        T=7,
-        Vmax=10,
+
+# Hay que tener en cuenta el tema del volumen
+def caso_base_T_r_Q(demandas: dict, lista_fechas: list, fecha_min, nombre_prod, T=7, Vmax=10,
         r=1,
         Q=1):
 
-    # r = trial.suggest_int('r', 1, 30)
-    # Q = trial.suggest_int('Q', 1, 30)
-
-    # constraint_volumen = y[]
     contador_dias_pasados = 0
     compras = dict()
     ventas = 0
@@ -74,9 +68,6 @@ def caso_base_T_r_Q(
     demanda_perdida = dict()
 
     inventario = {fecha_min - timedelta(days=1): 0}
-
-    # r, Q = 1, 7
-    # print(inventario)
 
     for fecha in lista_fechas:
         demanda_fecha = diccionario_demandas.get(fecha, 0)
@@ -94,7 +85,6 @@ def caso_base_T_r_Q(
             inventario[fecha] -= demanda_fecha
 
         if contador_dias_pasados % T == 0:
-            # print("REVISANDO FECHA")
             if inventario[fecha] < r:
                 ordenes_realizadas += 1
                 compras[fecha] = Q
@@ -105,19 +95,15 @@ def caso_base_T_r_Q(
     ventas_clp = ventas * precio_venta
     costo_comprar_clp = costo_compra * cantidad_comprada
     costo_fijo_clp = ordenes_realizadas * costo_fijo_comprar
-    costo_almacenaje_clp = sum(inventario.get(
-        fecha) * costo_almacenar for fecha in lista_fechas)
-    venta_perdida_clp = sum(demanda_perdida.values()) * \
-        (precio_venta - costo_compra)
+    costo_almacenaje_clp = sum(inventario.get(fecha) * costo_almacenar for fecha in lista_fechas)
+    venta_perdida_clp = sum(demanda_perdida.values()) * (precio_venta - costo_compra)
 
-    ganancias = ventas_clp - costo_comprar_clp - \
-        costo_fijo_clp - costo_almacenaje_clp - venta_perdida_clp
+    ganancias = ventas_clp - costo_comprar_clp - costo_fijo_clp - costo_almacenaje_clp - venta_perdida_clp
 
     print(f"[INFO]: Se vendieron en total de {ventas} unidades")
     print(f"[INFO]: Se hicieron en total {ordenes_realizadas} compras")
     print(f"[INFO]: Se tuvo un total de {quiebres_stock} quiebres de stock")
-    print(
-        f"[INFO]: La demanda perdida suma un total de {sum(demanda_perdida.values())}")
+    print(f"[INFO]: La demanda perdida suma un total de {sum(demanda_perdida.values())}")
     print(f"[INFO]: Se compraron en total {cantidad_comprada} productos")
     print(f"[INFO]: Las utilidades corresponden a {ganancias} CLP")
 
@@ -132,10 +118,13 @@ def caso_base_T_r_Q_optuna(trial,
                            T=7,
                            Vmax=10):
 
+
+    # hacer heurística para ver producto e ir comprando, como según un ranking
+    # Nunca nos dará si llenamos la bodega si política está bien calibrada
+
     r = trial.suggest_int('r', 1, 30)
     Q = trial.suggest_int('Q', 1, 30)
 
-    # constraint_volumen = y[]
     contador_dias_pasados = 0
     compras = dict()
     ventas = 0
@@ -145,9 +134,6 @@ def caso_base_T_r_Q_optuna(trial,
     demanda_perdida = dict()
 
     inventario = {fecha_min - timedelta(days=1): 0}
-
-    # r, Q = 1, 7
-    # print(inventario)
 
     for fecha in lista_fechas:
         demanda_fecha = diccionario_demandas.get(fecha, 0)
@@ -159,6 +145,13 @@ def caso_base_T_r_Q_optuna(trial,
         trial.set_user_attr("constraint", constraint_volumen)
         if inventario[fecha] < demanda_fecha:
             quiebres_stock += 1
+
+            # Ponerlo como supuesto. Comprar todo lo que hay es razonable
+
+            # No tenemos info quiebre stock histórico. Para nuestro análisis si tiene sentido
+            # que lo calculemos. En la realidad es difícil registrarlo con ese nivel de detalle
+            
+
             # Esto es como considerar que vendimos todo lo que teníamos
             # Esto hay que verlo bien y programarlo bien
             # Si nos llegan unidades de demanda y tenemos 5, vendemos las 5?
@@ -172,7 +165,6 @@ def caso_base_T_r_Q_optuna(trial,
             inventario[fecha] -= demanda_fecha
 
         if contador_dias_pasados % T == 0:
-            # print("REVISANDO FECHA")
             if inventario[fecha] < r:
                 ordenes_realizadas += 1
                 compras[fecha] = Q
@@ -184,15 +176,26 @@ def caso_base_T_r_Q_optuna(trial,
     costo_comprar_clp = costo_compra * cantidad_comprada
     costo_fijo_clp = ordenes_realizadas * costo_fijo_comprar
     costo_almacenaje_clp = sum(inventario.get(fecha) * costo_almacenar for fecha in lista_fechas)
-    venta_perdida_clp = sum(demanda_perdida.values()) * \
-        (precio_venta - costo_compra)
+    venta_perdida_clp = sum(demanda_perdida.values()) * (precio_venta - costo_compra)
 
-    ganancias = ventas_clp - costo_comprar_clp - \
-        costo_fijo_clp - costo_almacenaje_clp - venta_perdida_clp
-
+    ganancias = ventas_clp - costo_comprar_clp - costo_fijo_clp - costo_almacenaje_clp - venta_perdida_clp
 
     return ganancias
 
+    # Quizás le podemos poner un ponderador a la venta perdida. La función va a afectar mucho
+    # la calibración de la política. Estaría bueno para la entrega explicar bien lo de optuna
+    # Regla general: No correr algo que no se entiende que hace.
+    # Infinitas políticas en un espacio de dos dimensiones. Podemos poner distintas gráficos
+    # de la política, graficar la f.o en base a estos parámetros. Armar una superficie que optuna
+    # Aportaría mucho valor a la presentación, metodología de calibración
+
+    # E3: Análisis de sensibilidad parámetros que no podemos controlar que involucra decisiones externas
+    # Como tamaño de bodega por ejemplo.
+
+    # Graficar evolución del nivel de inventario estaría bueno
+    # Es muy indicativo de la dinámica del sistema. Ver cuando llegan las compras y cuando
+    # se hacen las compras. Indicarlo con puntito, flecha, debería verse distinto en productos
+    # con demanda distinta y leadtime distinto
 
 study = optuna.create_study(direction="maximize")
 objective = partial(caso_base_T_r_Q_optuna,
@@ -216,3 +219,10 @@ caso_base_T_r_Q(demandas=diccionario_demandas,
                 nombre_prod=articulo,
                 r=best_params["r"],
                 Q=best_params["Q"])
+
+
+#  Profundizar
+#  Vio harto trabajo el profe, orden, supimos juntar.
+#  
+#  - Cuantos productos usar
+#  - Como comparar
