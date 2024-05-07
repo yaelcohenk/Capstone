66a8d3195701f364c68dbf73f8259f6b000dd462
FRANCISCO SOLIS <fsolisgormaz@uc.cl>
2024-05-07 17:24:17 -0400

    Ordenando politica s,S

---
+++ src/funciones/parametros_producto.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def parametros_producto(datos_producto: pd.DataFrame):
+    precio_venta = datos_producto["unit_sale_price (CLP)"].to_numpy()[0]
+    costo_compra = datos_producto["cost (CLP)"].to_numpy()[0]
+    costo_almacenar = datos_producto["storage_cost (CLP)"].to_numpy()[0]
+    leadtime = int(datos_producto["leadtime (days)"].to_numpy()[0])
+    volumen = datos_producto["size_m3"].to_numpy()[0]
+    costo_fijo_comprar = datos_producto["cost_per_purchase"].to_numpy()[0]
+    item_id = datos_producto["item_id"].to_numpy()[0]
+    nombre_prod = datos_producto["description"].to_numpy()[0]
+
+    return precio_venta, costo_compra, costo_almacenar, leadtime, volumen, costo_fijo_comprar, item_id, nombre_prod
--- src/politica_s_S.py
+++ src/politica_s_S.py
@@ -1,3 +1,5 @@
+from functools import partial
+from datetime import timedelta
 import pandas as pd
 import os
 import optuna
@@ -5,42 +7,30 @@ import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-
-from datetime import timedelta
-from functools import partial
+from funciones.parametros_producto import parametros_producto
+from politicas.politica_s_S_eval import politica_T_s_S
 
 optuna.logging.set_verbosity(optuna.logging.WARNING)
-datos_test_item = pd.read_excel(os.path.join("datos", "data_items_fillna.xlsx"))
+datos_test_item = pd.read_excel(
+    os.path.join("datos", "data_items_fillna.xlsx"))
 datos_test_venta = pd.read_csv(os.path.join("datos", "data_sales.csv"), sep=";")
 datos_test_venta["date"] = pd.to_datetime(datos_test_venta["date"])
-# print(datos_test_venta)
 
 datos_2023_2024 = datos_test_venta[datos_test_venta["date"].dt.year >= 2023]
-# fecha_min = min(datos_2023_2024["date"])
+
 fecha_max = max(datos_2023_2024["date"])
-# Fecha en formato Year, mes, dia
-# print(f"Fecha más pequeña: {fecha_min}, tipo {type(fecha_min)}")
-# print(f"Fecha más alta: {fecha_max}")
 
-fecha_min = pd.Timestamp(year=2023, month=1, day=1) # Esta tendría que ser la fecha mínima
-# print(fecha_2023_inicio)
+# Esta tendría que ser la fecha mínima
+fecha_min = pd.Timestamp(year=2023, month=1, day=1)
 
-# sys.exit()
+
 articulo = "pro plan alimento seco para adulto razas medianas 15 kg"
 
 datos_test_item = datos_test_item[datos_test_item["description"].isin([
                                                                       articulo])]
 # Pasar todo esto a funciones
-precio_venta = datos_test_item["unit_sale_price (CLP)"].to_numpy()[0]
-costo_compra = datos_test_item["cost (CLP)"].to_numpy()[0]
-costo_almacenar = datos_test_item["storage_cost (CLP)"].to_numpy()[0]
-leadtime = int(datos_test_item["leadtime (days)"].to_numpy()[0])
-volumen = datos_test_item["size_m3"].to_numpy()[0]
-costo_fijo_comprar = datos_test_item["cost_per_purchase"].to_numpy()[0]
-
-item_id = datos_test_item["item_id"].to_numpy()[0]
-nombre_prod = datos_test_item["description"].to_numpy()[0]
+precio_venta, costo_compra, costo_almacenar, leadtime, volumen, costo_fijo_comprar, item_id, nombre_prod = parametros_producto(
+    datos_test_item)
 
 datos_test_venta["item_id"].replace({item_id: nombre_prod}, inplace=True)
 
@@ -50,14 +40,6 @@ datos_ventas["date"] = pd.to_datetime(datos_ventas["date"])
 
 
 datos_ventas = datos_ventas[datos_ventas["date"].dt.year >= 2023]
-
-
-# Acá en verdad hay que filtrar los datos y ocupar solo los datos del 2023 para adelante
-# por como se separaron los datos en el training
-
-# fecha_min = min(datos_ventas["date"]) # Más que la fecha del producto, debería ser la fecha de la primera venta registrada
-# fecha_max = max(datos_ventas["date"]) # Lo mismo acá. Eso o tengo que definir de antemano la primera fecha del año y buscar la última de 2024 que se haya vendido
-
 
 lista_fechas = [fecha_min + timedelta(days=i)
                 for i in range((fecha_max - fecha_min).days + 1)]
@@ -91,8 +73,6 @@ def caso_base_T_r_Q(demandas: dict, lista_fechas: list, fecha_min, nombre_prod, 
         inventario = inventario_fechas[fecha -
                                        timedelta(days=1)] + compras.get(fecha, 0)
 
-        # print(f"Al inicio del día {fecha} el inventario era {inventario}")
-        # print(f"El inventario el día de ayer era {inventario_fechas[fecha - timedelta(days=1)]} y hoy llegaron {compras.get(fecha, 0)} productos")
         if inventario < demanda_fecha:
             quiebres_stock += 1
             demanda_perdida[fecha] = demanda_fecha - inventario
@@ -106,16 +86,12 @@ def caso_base_T_r_Q(demandas: dict, lista_fechas: list, fecha_min, nombre_prod, 
             if inventario < s:
                 ordenes_realizadas += 1
                 cantidad_comprar = (S - inventario)
-                # print(f"La cantidad a comprar el día {fecha} y que llegará el {fecha + timedelta(days=leadtime)} es de {cantidad_comprar}\n")
                 lista_fechas_compras.append(fecha)
                 compras[fecha + timedelta(days=leadtime)] = cantidad_comprar
                 cantidad_comprada += cantidad_comprar
 
         contador_dias_pasados += 1
         inventario_fechas[fecha] = inventario
-        # print(f"Al final del día {fecha}")
-        # print(f"El inventario el día {fecha} es de {inventario}, este día llegaron {compras.get(fecha, 0)} productos comprados y la demanda fue de {demanda_fecha}\n")
-
 
     ventas_clp = ventas * precio_venta
     costo_comprar_clp = costo_compra * cantidad_comprada
@@ -131,8 +107,7 @@ def caso_base_T_r_Q(demandas: dict, lista_fechas: list, fecha_min, nombre_prod, 
     print(f"[INFO]: Se vendieron en total de {ventas} unidades")
     print(f"[INFO]: Se hicieron en total {ordenes_realizadas} compras")
     print(f"[INFO]: Se tuvo un total de {quiebres_stock} quiebres de stock")
-    print(
-        f"[INFO]: La demanda perdida suma un total de {sum(demanda_perdida.values())}")
+    print(f"[INFO]: La demanda perdida suma un total de {sum(demanda_perdida.values())}")
     print(f"[INFO]: Se compraron en total {cantidad_comprada} productos")
     print(f"[INFO]: Las utilidades corresponden a {ganancias} CLP")
 
@@ -146,10 +121,7 @@ def caso_base_T_r_Q_optuna(trial,
                            nombre_prod,
                            T=7,
                            Vmax=10):
-
-    # hacer heurística para ver producto e ir comprando, como según un ranking
-    # Nunca nos dará si llenamos la bodega si política está bien calibrada
-
+    
     s = trial.suggest_int('s', 1, 30)
     S = trial.suggest_int('S', s, 30)
 
@@ -168,8 +140,6 @@ def caso_base_T_r_Q_optuna(trial,
         inventario = inventario_fechas[fecha -
                                        timedelta(days=1)] + compras.get(fecha, 0)
 
-        # print(f"Al inicio del día {fecha} el inventario era {inventario}")
-        # print(f"El inventario el día de ayer era {inventario_fechas[fecha - timedelta(days=1)]} y hoy llegaron {compras.get(fecha, 0)} productos")
         if inventario < demanda_fecha:
             quiebres_stock += 1
             demanda_perdida[fecha] = demanda_fecha - inventario
@@ -183,7 +153,6 @@ def caso_base_T_r_Q_optuna(trial,
             if inventario < s:
                 ordenes_realizadas += 1
                 cantidad_comprar = (S - inventario)
-                # print(f"La cantidad a comprar el día {fecha} y que llegará el {fecha + timedelta(days=leadtime)} es de {cantidad_comprar}\n")
 
                 compras[fecha + timedelta(days=leadtime)] = cantidad_comprar
                 cantidad_comprada += cantidad_comprar
@@ -204,6 +173,7 @@ def caso_base_T_r_Q_optuna(trial,
 
     return ganancias
 
+
 study = optuna.create_study(direction="maximize")
 objective = partial(caso_base_T_r_Q_optuna,
                     demandas=diccionario_demandas,
@@ -220,15 +190,20 @@ print("Optimized function value:", best_value)
 
 print(f"[INFO]: Para {best_params} se obtiene")
 
-ganancias, fechas, inventario_caso, compras, fechas_compras = caso_base_T_r_Q(demandas=diccionario_demandas,
-                                                lista_fechas=lista_fechas,
-                                                fecha_min=fecha_min,
-                                                nombre_prod=articulo,
-                                                s=best_params["s"],
-                                                S=best_params["S"])
 
 
 
+ganancias, fechas, inventario_caso, compras, fechas_compras = politica_T_s_S(diccionario_demandas=diccionario_demandas,
+                                                                             lista_fechas=lista_fechas,
+                                                                             fecha_min=fecha_min,
+                                                                             nombre_prod="",
+                                                                             leadtime=leadtime,
+                                                                             precio_venta=precio_venta,
+                                                                             costo_fijo_comprar=costo_fijo_comprar,
+                                                                             costo_compra=costo_compra,
+                                                                             costo_almacenar=costo_almacenar,
+                                                                             s=best_params["s"],
+                                                                             S=best_params["S"])
 
 
 fig_contour = optuna.visualization.plot_contour(study, params=["s", "S"])
@@ -238,7 +213,7 @@ fig_contour.show()
 
 
 plt.figure(figsize=(20, 6))
-sns.lineplot(x=fechas,y=inventario_caso)
+sns.lineplot(x=fechas, y=inventario_caso)
 plt.xlabel("Fechas")
 plt.ylabel("Cantidad de inventario (unidades)")
 plt.title(f"Inventario a través timempo del producto X")
@@ -246,6 +221,6 @@ plt.title(f"Inventario a través timempo del producto X")
 
 # Como que con lo de abajo igual se ve medio feo, pero preguntarle al profe
 # for compra in fechas_compras:
-    # plt.axvline(x=compra, color = 'r')
+# plt.axvline(x=compra, color = 'r')
 
 # plt.show()
